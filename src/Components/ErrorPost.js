47ad77054c4e612d2a79a6996add3b98d06c3c2e
basil103 <bzoneof93@gmail.com>
2024-09-12 15:33:18 +0400

    more fix UI & load errors indicators

--- src/Components/ErrorPost.js
+++ src/Components/ErrorPost.js
@@ -1,14 +1,24 @@
 import Post from "./Post";
 import styles from '../ComponentsStyles/LoadPost.module.css';
+import { useState, useEffect } from "react";
 
 function ErrorPost({mode}){
+    const [show, setShow] = useState(`${styles.dummyError}`);
+    
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{setShow(`${styles.dummyError} ${styles.show}`)}, 3500);
+        return ()=>{
+            clearTimeout(timeout);
+        }
+    },[]);
+    
     return(
     <>
         <div className={!mode ? styles.loadPostContainer : styles.loadPostContainerDark}>
                 <div className={styles.loadBufferError}>
                     <div className={styles.dummyHolder}>
-                        <div className={styles.dummyError}>
-                            <span>Something went wrong!</span>
+                        <div>
+                            <span className={show}>Something went wrong!</span>
                         </div>
                         <Post hide={true}/>
                     </div>
